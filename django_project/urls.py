eeff225dde4d8fb0f07456bc47c04a1422ff603b
Gabriel Menendez <menendez.gabriel@gmail.com>
2024-02-29 21:07:25 -0300

    add uplaod files

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,4 +25,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
     path("books/", include("books.urls"))
-]
+]+ static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
